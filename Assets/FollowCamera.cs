72e0b9cc51ba461ea496ed6c9bd9f7da8c2b108a
Kyrune <jn-01@centurytel.net>
2021-10-06 14:09:38 -0500

    Add camera position in relation to car object

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -10,6 +10,6 @@ public class FollowCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = thingToFollow.transform.position + new Vector3 (0, 0, -10);
     }
 }
