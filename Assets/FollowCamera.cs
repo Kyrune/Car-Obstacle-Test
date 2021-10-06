a7c5b2ac5a58947587a7e5a5478b67f33d12567e
Kyrune <jn-01@centurytel.net>
2021-10-06 13:58:33 -0500

    Remove start method

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -6,11 +6,7 @@ public class FollowCamera : MonoBehaviour
 {
     // The camera's position is relative to the car's
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
