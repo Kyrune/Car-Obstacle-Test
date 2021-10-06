0efe554b797a95dff6b15c9dc82f7d6d25ab13ec
Kyrune <jn-01@centurytel.net>
2021-10-06 14:02:34 -0500

    Remove start comment

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     // The camera's position is relative to the car's
-    // Start is called before the first frame update
-    
+
     // Update is called once per frame
     void Update()
     {
