6f7a7f7c658558217ab5fc72da6d322363ba99c1
Kyrune <jn-01@centurytel.net>
2021-10-07 13:23:40 -0500

    Boost speed on Trigger

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -9,16 +9,23 @@ public class Driver : MonoBehaviour
     [SerializeField] float slowSpeed = 10f;
     [SerializeField] float boostSpeed = 30f;
 
-    void OnCollisionEnter2D(Collision2D other) 
-    {
-        moveSpeed = slowSpeed;
-    }
-
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount); 
         transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        moveSpeed = slowSpeed;
+    }
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.tag == "Speed Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
